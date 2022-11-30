6075f9413baf60cdadec0d070bb05157802a5584
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2022-11-30 11:02:05 -0800

    updated promises to async/await

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const img = document.getElementById('img')
 
 btn.addEventListener('click', getDog)
 
-function getDog() {
-    fetch('https://random.dog/woof.json')
-        .then(res => res.json()) 
-        .then(data => {
-            if (data.url.includes('.mp4')) {
-                getDog()
-            } else {
-                img.innerHTML = `<img src="${data.url}" alt="" />`
-            }
-        })
+async function getDog() {
+    const res = await fetch('https://random.dog/woof.json')
+    const data = await res.json()
+    if (data.url.includes('.mp4')) {
+        getDog()
+    } else {
+        img.innerHTML = `<img src="${data.url}" alt="" />`
+    }
 }
